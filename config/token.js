f021acc2619a1bfb36b313d3edc0ede85635dce2
awggardner <100781000+awggardner@users.noreply.github.com>
2022-05-24 14:52:57 +0100

    added cookie into config

--- config/token.js
+++ config/token.js
@@ -4,7 +4,9 @@ const jwt = require('jsonwebtoken');
 dotenv.config();
 
 const SECRET = process.env.TOKEN_SECRET;
+const REFRESH_SECRET = process.env.REFRESH_SECRET;
 const JWT_TIMEOUT = (60 * 30); // 60 seconds * 30
+const MS_PER_MINUTE = 3600;
 
 /** 
 * Returns a signed Json Web Token (JWT) token for the given unique identifier, a username in an object. This token will be valid for
@@ -15,6 +17,28 @@ const JWT_TIMEOUT = (60 * 30); // 60 seconds * 30
 */
 function generateAccessToken(username, role) {
     return jwt.sign({ username, role }, SECRET, { expiresIn: JWT_TIMEOUT });
+}
+
+function generateRefreshToken(username, role) {
+    return jwt.sign({ username, role }, REFRESH_SECRET, {expiresIn: "1d" });
+}
+
+function refreshAccessTokenMiddleware(request, response, next) {
+    const refreshToken = request.cookies.refreshToken;
+
+    if (refreshToken) {
+        jwt.verify(refreshToken, REFRESH_SECRET, (error, token) => {
+            if (error) {
+                return response.status(403).send('Invalid refresh token supplied');
+            }
+            const user = request.user;
+            const newToken = generateAccessToken(user.username, user.role);
+
+            return response.status(200).json({ newToken, expiration: JWT_TIMEOUT, user});
+        });
+    } else {
+        return response.status(400).send('No refresh token supplied');
+    }
 }
 
 /**
@@ -65,7 +89,9 @@ function authenticationMiddleware(request, response, next) {
 
 module.exports = {
     generateAccessToken,
+    generateRefreshToken,
     authenticationMiddleware,
+    refreshAccessTokenMiddleware,
     isAdmin,
     JWT_TIMEOUT
 }
