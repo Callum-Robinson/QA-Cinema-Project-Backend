7bec0b28ea022122efae098a33ef1d5aa2797b5b
Mohab <khalifamohab97@outlook.com>
2022-05-24 14:19:53 +0100

    added get all request

--- api/router/booking-router.js
+++ api/router/booking-router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bookingController = require('../controller/booking-controller');
 const router = express.Router();
 
-// router.get('/', bookingController.getAllBookings);
+router.get('/', bookingController.getAllBookings);
 router.get('/:id', bookingController.getBookingById);
 router.post('/', bookingController.createBooking);
 // router.put('/:id', bookingController.updateBooking);
