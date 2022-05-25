a0e98968555f730ea25f25b7c641dbebb77c38c7
Callum <callumjrobinson2020@gmail.com>
2022-05-25 14:19:58 +0100

    added needed imports to new release controller

---
+++ api/controller/new-release-controller.js
@@ -0,0 +1,5 @@
+const NewMovieNotFoundError = require('../errors/new-movie-not-found-error.js');
+const NewRelease = require("../model/newMovie.js");
+
+const fs = require("fs");
+const path = require("path");
