35b9bb201607b44006ff3e88757f1500e3464b74
Callum <callumjrobinson2020@gmail.com>
2022-05-25 14:37:14 +0100

    added read paths to new release controller

--- api/controller/new-release-controller.js
+++ api/controller/new-release-controller.js
@@ -3,3 +3,24 @@ const NewRelease = require("../model/newMovie.js");
 
 const fs = require("fs");
 const path = require("path");
+
+module.exports = {
+
+    // Read all
+    getAllNewReleases: async (req, res, next) => {
+        const releases = await NewRelease.find({});
+        res.status(200).json(releases);
+    },
+
+    // Read by id
+    getNewReleaseById: async (req, res, next) => {
+        const id = req.params.id;
+        const release = await NewRelease.findById(id);
+
+        if (movie) {
+            res.status(200).json(release);
+            return;
+        }
+        next(new NewMovieNotFoundError(id));
+    },
+}
