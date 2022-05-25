b7bea51a5680ec6a665376930f235f4a3a705fea
Callum <callumjrobinson2020@gmail.com>
2022-05-25 15:55:13 +0100

    added multer set up to new release router

--- api/router/new-release-router.js
+++ api/router/new-release-router.js
@@ -3,3 +3,14 @@ const express = require('express');
 const newReleaseController = require('../controller/new-release-controller');
 const router = express.Router();
 
+// Set up multer for storing files
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './api/controller/uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+const upload = multer({ storage: storage });
