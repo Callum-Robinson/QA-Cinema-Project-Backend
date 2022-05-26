15f1620abe00dcf50e4a7a96dd95c0d78f24f8b8
awggardner <100781000+awggardner@users.noreply.github.com>
2022-05-26 18:33:49 +0100

    added eventlistner to search input

---
+++ searchbar/script.js
@@ -0,0 +1,20 @@
+const movieCardTemplate = document.querySelector("[data-movie-template]")
+const movieCardContainer = document.querySelector("[data-movie-cards-container]")
+const searchInput = document.querySelector("[data-search")
+
+searchInput.addEventListener("input")
+
+fetch("localhost:5000/movie")
+.then(res => res.json())
+.then(data => {
+    data.forEach(movie => {
+        //fetches movies from database and then fills information into cards to display
+        const card = movieCardTemplate.textContent.cloneNode(true).children[0]
+        const header = card.querySelector("[data-header")
+        const body = card.querySelector("[data-body")
+        header.textContent = movie.title
+        body.textContent = movie.description
+        movieCardContainer.append(card)
+        console.log(movie);
+    })    
+})
