5f27af0408f7439a6bbe2076e4f3bac853b893d1
awggardner <100781000+awggardner@users.noreply.github.com>
2022-05-26 18:42:38 +0100

    created an empty array to hold movie and set return to give back title and description

--- searchbar/script.js
+++ searchbar/script.js
@@ -2,12 +2,18 @@ const movieCardTemplate = document.querySelector("[data-movie-template]")
 const movieCardContainer = document.querySelector("[data-movie-cards-container]")
 const searchInput = document.querySelector("[data-search")
 
-searchInput.addEventListener("input")
+searchInput.addEventListener("input", () => {
+    const value = e.target.value
+    console.log(value)
+    
+})
+
+let movie = []
 
 fetch("localhost:5000/movie")
 .then(res => res.json())
 .then(data => {
-    data.forEach(movie => {
+    movie = data.map(movie => {
         //fetches movies from database and then fills information into cards to display
         const card = movieCardTemplate.textContent.cloneNode(true).children[0]
         const header = card.querySelector("[data-header")
@@ -15,6 +21,6 @@ fetch("localhost:5000/movie")
         header.textContent = movie.title
         body.textContent = movie.description
         movieCardContainer.append(card)
-        console.log(movie);
+        return { title: movie.title, description: movie.description, element: card }
     })    
 })
