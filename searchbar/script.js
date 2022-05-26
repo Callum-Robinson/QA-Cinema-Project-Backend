bb0b9d0f056415bab02fe81b0222ad30770b90fb
awggardner <100781000+awggardner@users.noreply.github.com>
2022-05-26 19:22:19 +0100

    followed a tutorial and got a searchbar done, will adjust the css and add in more fields for the cards to display if I have time tomorrow

--- searchbar/script.js
+++ searchbar/script.js
@@ -1,19 +1,27 @@
+// thanks to Web Dev Simplified - https://youtu.be/TlP5WIxVirU 
+
+const movie = require("../api/model/movie")
+
 const movieCardTemplate = document.querySelector("[data-movie-template]")
 const movieCardContainer = document.querySelector("[data-movie-cards-container]")
 const searchInput = document.querySelector("[data-search")
 
 searchInput.addEventListener("input", () => {
-    const value = e.target.value
-    console.log(value)
+    const value = e.target.value.toLowerCase()
+    movies.forEach(user => {
+        const isVisible = movie.title.toLowerCase().includes(value) || 
+        movie.description.toLowerCase().includes(value)
+        movie.element.classList.toggle("hide", !isVisible)
+    })
     
 })
 
-let movie = []
+let movies = []
 
-fetch("localhost:5000/movie")
+fetch("http://localhost:5000/movie")
 .then(res => res.json())
 .then(data => {
-    movie = data.map(movie => {
+    movies = data.map(movie => {
         //fetches movies from database and then fills information into cards to display
         const card = movieCardTemplate.textContent.cloneNode(true).children[0]
         const header = card.querySelector("[data-header")
